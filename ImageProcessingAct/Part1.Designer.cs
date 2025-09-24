dfda3b2706c22270fa12179e1d10ff607d5c35fd
BladeLucas27 <raymondgtio@gmail.com>
2025-09-24 09:21:24 +0800

    Changed Histogram to display independently rather than being a specific button.

--- ImageProcessingAct/Part1.Designer.cs
+++ ImageProcessingAct/Part1.Designer.cs
@@ -39,25 +39,28 @@
             this.pictureBoxOriginal = new System.Windows.Forms.PictureBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.pictureBoxHistogram = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEdited)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHistogram)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonSave
             // 
-            this.buttonSave.Location = new System.Drawing.Point(828, 30);
-            this.buttonSave.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonSave.Location = new System.Drawing.Point(758, 25);
+            this.buttonSave.Margin = new System.Windows.Forms.Padding(2);
             this.buttonSave.Name = "buttonSave";
             this.buttonSave.Size = new System.Drawing.Size(75, 23);
             this.buttonSave.TabIndex = 22;
             this.buttonSave.Text = "Save Image";
             this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click_1);
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(503, 34);
-            this.checkBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkBox1.Location = new System.Drawing.Point(433, 29);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(2);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(98, 17);
             this.checkBox1.TabIndex = 21;
@@ -67,7 +70,7 @@
             // 
             // buttonSepia
             // 
-            this.buttonSepia.Location = new System.Drawing.Point(828, 61);
+            this.buttonSepia.Location = new System.Drawing.Point(758, 56);
             this.buttonSepia.Name = "buttonSepia";
             this.buttonSepia.Size = new System.Drawing.Size(75, 23);
             this.buttonSepia.TabIndex = 20;
@@ -77,7 +80,7 @@
             // 
             // buttonInvert
             // 
-            this.buttonInvert.Location = new System.Drawing.Point(663, 61);
+            this.buttonInvert.Location = new System.Drawing.Point(593, 56);
             this.buttonInvert.Name = "buttonInvert";
             this.buttonInvert.Size = new System.Drawing.Size(75, 23);
             this.buttonInvert.TabIndex = 18;
@@ -87,7 +90,7 @@
             // 
             // buttonGrayScale
             // 
-            this.buttonGrayScale.Location = new System.Drawing.Point(583, 61);
+            this.buttonGrayScale.Location = new System.Drawing.Point(513, 56);
             this.buttonGrayScale.Name = "buttonGrayScale";
             this.buttonGrayScale.Size = new System.Drawing.Size(75, 23);
             this.buttonGrayScale.TabIndex = 17;
@@ -97,7 +100,7 @@
             // 
             // buttonCopy
             // 
-            this.buttonCopy.Location = new System.Drawing.Point(503, 61);
+            this.buttonCopy.Location = new System.Drawing.Point(433, 56);
             this.buttonCopy.Name = "buttonCopy";
             this.buttonCopy.Size = new System.Drawing.Size(75, 23);
             this.buttonCopy.TabIndex = 16;
@@ -107,7 +110,7 @@
             // 
             // buttonLoad
             // 
-            this.buttonLoad.Location = new System.Drawing.Point(221, 61);
+            this.buttonLoad.Location = new System.Drawing.Point(168, 56);
             this.buttonLoad.Name = "buttonLoad";
             this.buttonLoad.Size = new System.Drawing.Size(75, 23);
             this.buttonLoad.TabIndex = 15;
@@ -118,7 +121,7 @@
             // pictureBoxEdited
             // 
             this.pictureBoxEdited.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBoxEdited.Location = new System.Drawing.Point(503, 103);
+            this.pictureBoxEdited.Location = new System.Drawing.Point(433, 98);
             this.pictureBoxEdited.Name = "pictureBoxEdited";
             this.pictureBoxEdited.Size = new System.Drawing.Size(400, 400);
             this.pictureBoxEdited.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -129,7 +132,7 @@
             // pictureBoxOriginal
             // 
             this.pictureBoxOriginal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBoxOriginal.Location = new System.Drawing.Point(68, 103);
+            this.pictureBoxOriginal.Location = new System.Drawing.Point(15, 98);
             this.pictureBoxOriginal.Name = "pictureBoxOriginal";
             this.pictureBoxOriginal.Size = new System.Drawing.Size(400, 400);
             this.pictureBoxOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -141,10 +144,20 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // pictureBoxHistogram
+            // 
+            this.pictureBoxHistogram.Location = new System.Drawing.Point(856, 98);
+            this.pictureBoxHistogram.Name = "pictureBoxHistogram";
+            this.pictureBoxHistogram.Size = new System.Drawing.Size(400, 400);
+            this.pictureBoxHistogram.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxHistogram.TabIndex = 23;
+            this.pictureBoxHistogram.TabStop = false;
+            // 
             // Part1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pictureBoxHistogram);
             this.Controls.Add(this.buttonSave);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.buttonSepia);
@@ -154,12 +167,13 @@
             this.Controls.Add(this.buttonLoad);
             this.Controls.Add(this.pictureBoxEdited);
             this.Controls.Add(this.pictureBoxOriginal);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Part1";
-            this.Size = new System.Drawing.Size(971, 532);
+            this.Size = new System.Drawing.Size(1273, 532);
             this.Load += new System.EventHandler(this.Part1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEdited)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHistogram)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -177,5 +191,6 @@
         private System.Windows.Forms.PictureBox pictureBoxOriginal;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBoxHistogram;
     }
 }
--- ImageProcessingAct/Part1.cs
+++ ImageProcessingAct/Part1.cs
@@ -27,48 +27,6 @@ namespace ImageProcessingAct
 
         }
 
-        //private void buttonHistogram_Click(object sender, EventArgs e)
-        //{
-        //    Graphics g = pictureBoxEdited.CreateGraphics();
-        //    g.Clear(Color.White);
-        //    if (pictureBoxOriginal.Image != null)
-        //    {
-        //        int[] histogram = new int[256];
-        //        for (int y = 0; y < originalHeight; y++)
-        //        {
-        //            for (int x = 0; x < originalWidth; x++)
-        //            {
-        //                Color originalColor = originalBitmap.GetPixel(x, y);
-        //                int grayValue = (int)((originalColor.R + originalColor.G + originalColor.B) / 3);
-        //                histogram[grayValue]++;
-        //            }
-        //        }
-        //        int max = histogram.Max();
-        //        for (int i = 0; i < 256; i++)
-        //        {
-        //            int barHeight = (int)((histogram[i] / (float)max) * pictureBoxEdited.Height);
-        //            g.DrawLine(Pens.Black, i * 2, pictureBoxEdited.Height, i * 2, pictureBoxEdited.Height - barHeight);
-        //        }
-        //    }
-        //}
-        private void buttonSave_Click(object sender, EventArgs e)
-        {
-            if (pictureBoxEdited.Image != null)
-            {
-                saveFileDialog1.Filter = "PNG Image|*.png|JPEG Image|*.jpg;*.jpeg|Bitmap Image|*.bmp|GIF Image|*.gif";
-                saveFileDialog1.Title = "Save the Edited Image";
-                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
-                {
-                    string saveFilePath = saveFileDialog1.FileName;
-                    pictureBoxEdited.Image.Save(saveFilePath);
-                }
-            }
-        }
-        private void checkBox2_CheckedChanged(object sender, EventArgs e)
-        {
-            ignoreGreen = !ignoreGreen;
-        }
-
         private void reverseImage()
         {
             reversedBitmap = new Bitmap(originalBitmap);
@@ -80,6 +38,90 @@ namespace ImageProcessingAct
                     reversedBitmap.SetPixel(originalWidth - 1 - x, y, originalColor);
                 }
             }
+        }
+        private void ShowRGBHistogram(Bitmap bitmap)
+        {
+            if (bitmap == null || pictureBoxHistogram == null) return;
+
+            int[] redHistogram = new int[256];
+            int[] greenHistogram = new int[256];
+            int[] blueHistogram = new int[256];
+
+            for (int y = 0; y < bitmap.Height; y++)
+            {
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    Color pixel = bitmap.GetPixel(x, y);
+                    redHistogram[pixel.R]++;
+                    greenHistogram[pixel.G]++;
+                    blueHistogram[pixel.B]++;
+                }
+            }
+
+            int max = Math.Max(redHistogram.Max(), Math.Max(greenHistogram.Max(), blueHistogram.Max()));
+            int width = 256;
+            int height = pictureBoxHistogram.Height > 0 ? pictureBoxHistogram.Height : 100;
+            int marginLeft = 30;
+            int marginBottom = 20;
+            int graphWidth = width;
+            int graphHeight = height - marginBottom;
+
+            Bitmap histImage = new Bitmap(width + marginLeft, height);
+            using (Graphics g = Graphics.FromImage(histImage))
+            {
+                g.Clear(Color.White);
+
+                // Draw gridlines and Y-axis labels
+                int gridLines = 5;
+                using (Pen gridPen = new Pen(Color.LightGray, 1))
+                using (Font labelFont = new Font("Arial", 8))
+                using (Brush labelBrush = new SolidBrush(Color.Black))
+                {
+                    for (int i = 0; i <= gridLines; i++)
+                    {
+                        int y = marginBottom + (int)((graphHeight) * (1 - i / (float)gridLines));
+                        g.DrawLine(gridPen, marginLeft, y, marginLeft + graphWidth, y);
+                        int labelValue = (int)(max * i / (float)gridLines);
+                        g.DrawString(labelValue.ToString(), labelFont, labelBrush, 0, y - 8);
+                    }
+                }
+
+                // Draw X-axis labels (0, 64, 128, 192, 255)
+                using (Font labelFont = new Font("Arial", 8))
+                using (Brush labelBrush = new SolidBrush(Color.Black))
+                {
+                    int[] xLabels = { 0, 64, 128, 192, 255 };
+                    foreach (int xLabel in xLabels)
+                    {
+                        int x = marginLeft + xLabel;
+                        g.DrawString(xLabel.ToString(), labelFont, labelBrush, x - 10, height - marginBottom + 2);
+                    }
+                }
+
+                // Draw axes
+                using (Pen axisPen = new Pen(Color.Black, 2))
+                {
+                    // Y-axis
+                    g.DrawLine(axisPen, marginLeft, marginBottom, marginLeft, height - marginBottom);
+                    // X-axis
+                    g.DrawLine(axisPen, marginLeft, height - marginBottom, marginLeft + graphWidth, height - marginBottom);
+                }
+
+                // Draw histogram bars
+                for (int i = 0; i < 256; i++)
+                {
+                    int rHeight = (int)((redHistogram[i] / (float)max) * graphHeight);
+                    int gHeight = (int)((greenHistogram[i] / (float)max) * graphHeight);
+                    int bHeight = (int)((blueHistogram[i] / (float)max) * graphHeight);
+
+                    int x = marginLeft + i;
+                    g.DrawLine(Pens.Red, x, height - marginBottom, x, height - marginBottom - rHeight);
+                    g.DrawLine(Pens.Green, x, height - marginBottom, x, height - marginBottom - gHeight);
+                    g.DrawLine(Pens.Blue, x, height - marginBottom, x, height - marginBottom - bHeight);
+                }
+            }
+            pictureBoxHistogram.Image = histImage;
+            pictureBoxHistogram.Refresh();
         }
 
         private void buttonLoad_Click_1(object sender, EventArgs e)
@@ -127,6 +169,7 @@ namespace ImageProcessingAct
                     }
                 }
                 pictureBoxEdited.Image = editedBitmap;
+                ShowRGBHistogram(editedBitmap);
             }
         }
 
@@ -162,6 +205,7 @@ namespace ImageProcessingAct
                     }
                 }
                 pictureBoxEdited.Image = editedBitmap;
+                ShowRGBHistogram(editedBitmap);
             }
         }
 
@@ -196,6 +240,7 @@ namespace ImageProcessingAct
                     }
                 }
                 pictureBoxEdited.Image = editedBitmap;
+                ShowRGBHistogram(editedBitmap);
             }
         }
 
@@ -229,12 +274,27 @@ namespace ImageProcessingAct
                     }
                 }
                 pictureBoxEdited.Image = editedBitmap;
+                ShowRGBHistogram(editedBitmap);
             }
         }
 
         private void checkBox1_CheckedChanged_1(object sender, EventArgs e)
         {
             isReversed = !isReversed;
+        }
+
+        private void buttonSave_Click_1(object sender, EventArgs e)
+        {
+            if (pictureBoxEdited.Image != null)
+            {
+                saveFileDialog1.Filter = "PNG Image|*.png|JPEG Image|*.jpg;*.jpeg|Bitmap Image|*.bmp|GIF Image|*.gif";
+                saveFileDialog1.Title = "Save the Edited Image";
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    string saveFilePath = saveFileDialog1.FileName;
+                    pictureBoxEdited.Image.Save(saveFilePath);
+                }
+            }
         }
 
         private void pictureBoxOriginal_Click_1(object sender, EventArgs e)
